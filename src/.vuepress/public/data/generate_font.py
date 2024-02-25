aad88c5367bd7f8606f95dd018726fd07344770f
brangpd <brangpd@outlook.com>
2024-02-25 14:39:01 +0800

    给生成的字体换个名字；加一个存 NotoSans 字体子集的脚本备用
    （根据OFL协议，修改后字体不能用原来的名称）

--- src/.vuepress/public/data/generate_font.py
+++ src/.vuepress/public/data/generate_font.py
@@ -5,6 +5,7 @@ import sys
 import tempfile
 import urllib.request
 import fontTools.merge
+from fontname import main as fontname
 from fontTools.subset import main as pyftsubset
 from fontTools.ttLib.woff2 import compress as woff2_compress
 from puj import *
@@ -17,18 +18,43 @@ def is_cjk_basic(c):
 
 
 TEMP_DIR = tempfile.mkdtemp()
+
+NOTO_SANS_SC_PATH = os.path.join(FONTS_DIR, 'NotoSansSC-Regular.otf')
+BASIC_SUBSET_FONT_NAME_OTF = os.path.join(TEMP_DIR, 'NotoSansSC-Regular.generated.otf')
+BASIC_SUBSET_FONT_NAME_WOFF2 = os.path.join(TEMP_DIR, 'NotoSansSC-Regular.generated.woff2')
+
 PLANGOTHIC_PATH = os.path.join(FONTS_DIR, 'Plangothic.ttc')
+SUBSET_FONT_NAME = 'CJKExtSubset'
 SUBSET_FONT_NAME_TTF0 = os.path.join(TEMP_DIR, '0-CJKExtSubset.generated.ttf')
 SUBSET_FONT_NAME_TTF1 = os.path.join(TEMP_DIR, '1-CJKExtSubset.generated.ttf')
 SUBSET_FONT_NAME_TTF = os.path.join(TEMP_DIR, 'CJKExtSubset.generated.ttf')
 SUBSET_FONT_NAME_WOFF2 = os.path.join(TEMP_DIR, 'CJKExtSubset.generated.woff2')
-FINAL_WOFF2_PATH = os.path.join(FONTS_DIR, 'CJKExtSubset.generated.woff2')
+
+
+def ensure_noto_sans_sc():
+    if not os.path.exists(NOTO_SANS_SC_PATH):
+        url = 'https://github.com/notofonts/noto-cjk/raw/main/Sans/SubsetOTF/SC/NotoSansSC-Regular.otf'
+        urllib.request.urlretrieve(url, NOTO_SANS_SC_PATH)
 
 
 def ensure_plangothic():
     if not os.path.exists(PLANGOTHIC_PATH):
         url = 'https://github.com/Fitzgerald-Porthmouth-Koenigsegg/Plangothic-Project/raw/main/Plangothic.ttc'
         urllib.request.urlretrieve(url, PLANGOTHIC_PATH)
+
+
+def basic_subset():
+    if os.path.exists(BASIC_SUBSET_FONT_NAME_OTF):
+        os.remove(BASIC_SUBSET_FONT_NAME_OTF)
+    pyftsubset([
+        NOTO_SANS_SC_PATH,
+        f'--unicodes=U+4E00-9FFF,U+3400-4DBF',
+        f'--output-file={BASIC_SUBSET_FONT_NAME_OTF}',
+    ])
+
+    woff2_compress(BASIC_SUBSET_FONT_NAME_OTF, BASIC_SUBSET_FONT_NAME_WOFF2)
+
+    shutil.copy(BASIC_SUBSET_FONT_NAME_WOFF2, FONTS_DIR)
 
 
 def subset(chars):
@@ -51,6 +77,7 @@ def subset(chars):
     merger = fontTools.merge.Merger()
     font = merger.merge([f'{SUBSET_FONT_NAME_TTF0}', f'{SUBSET_FONT_NAME_TTF1}'])
     font.save(SUBSET_FONT_NAME_TTF)
+    fontname([SUBSET_FONT_NAME, SUBSET_FONT_NAME_TTF])
 
     if os.path.exists(SUBSET_FONT_NAME_WOFF2):
         os.remove(SUBSET_FONT_NAME_WOFF2)
@@ -83,6 +110,8 @@ def get_chars():
 
 if __name__ == '__main__':
     chars = get_chars()
+    # ensure_noto_sans_sc()
     ensure_plangothic()
+    # basic_subset()
     subset(chars)
     post_process()
