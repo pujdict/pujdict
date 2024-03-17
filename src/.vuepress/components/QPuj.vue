b4788b503369a787a06d1854e365ce5dfa58f92e
brangpd <brangpd@outlook.com>
2024-03-17 16:09:17 +0800

    修复轻声调的--捕获

--- src/.vuepress/components/QPuj.vue
+++ src/.vuepress/components/QPuj.vue
@@ -547,7 +547,7 @@ function addPUJToneMarkSentence(sentence) {
         result += cur;
       });
   // 为了美观轻声调用点表示
-  result = result.replace(/(?<=\w)--(?=\w)/g, '·');
+  result = result.replace(/(?<=\S)--(?=\S)/g, '·');
   return result;
 }
 
