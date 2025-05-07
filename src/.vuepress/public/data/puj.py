f68a210e9059021898497a713a96b268ef0c8342
brangpd <brangpd@outlook.com>
2025-05-07 22:28:26 +0800

    适配字表添加 sp_nasal 一列。

--- src/.vuepress/public/data/puj.py
+++ src/.vuepress/public/data/puj.py
@@ -2,7 +2,7 @@ entry_index = 0
 
 
 class Entry:
-    def __init__(self, char, char_sim, initial, final, tone, cat, char_ref, details):
+    def __init__(self, char, char_sim, initial, final, tone, sp_nasal, cat, char_ref, details):
         global entry_index
         entry_index += 1
         self.entry_index = str(entry_index)
@@ -10,6 +10,7 @@ class Entry:
         self.char_sim = char_sim
         self.initial = initial
         self.final = final
+        self.sp_nasal = sp_nasal
         self.combination = initial + final + tone
         self.tone = tone
         self.cat = cat
@@ -17,4 +18,4 @@ class Entry:
         self.details = details
 
     def __str__(self):
-        return self.entry_index + '\t' + self.char + '\t' + self.char_sim + '\t' + self.initial + '\t' + self.final + '\t' + self.tone + '\t' + self.cat + '\t' + self.char_ref + '\t' + self.details
+        return self.entry_index + '\t' + self.char + '\t' + self.char_sim + '\t' + self.initial + '\t' + self.final + '\t' + self.sp_nasal + '\t' + self.tone + '\t' + self.cat + '\t' + self.char_ref + '\t' + self.details
