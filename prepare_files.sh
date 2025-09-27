2e27a5d2a0f205dead394360c1c453de08ea154e
brangpd <brangpd@outlook.com>
2025-09-27 19:31:10 +0800

    更新主页说明。

--- prepare_files.sh
+++ prepare_files.sh
@@ -6,7 +6,8 @@ cd src/.vuepress/public/data
 if [ -d pujdict-base ]; then
   git pull || exit 1
 else
-  git clone --depth 1 https://github.com/pujdict/pujdict-base.git || exit 1
+  origin=$(git remote get-url origin)
+  git clone --depth 1 ${origin%/*}/pujdict-base.git || exit 1
 fi
 
 python3 generate_font.py < /dev/null
