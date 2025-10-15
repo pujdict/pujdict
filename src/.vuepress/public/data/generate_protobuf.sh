4f972c89ceafaabea44795ffab957a0c3387f3a1
brangpd <brangpd@outlook.com>
2025-10-15 22:27:22 +0800

    适配 pujdict-base 的项目结构更改：src -> libpuj。

--- src/.vuepress/public/data/generate_protobuf.sh
+++ src/.vuepress/public/data/generate_protobuf.sh
@@ -1,8 +1,8 @@
 set -e
 pbjs -t static-module -w es6 -o SPujPb.js \
- pujdict-base/src/entries.proto \
- pujdict-base/src/accents.proto \
- pujdict-base/src/phrases.proto
+ pujdict-base/libpuj/entries.proto \
+ pujdict-base/libpuj/accents.proto \
+ pujdict-base/libpuj/phrases.proto
 
 pbts -o SPujPb.d.ts SPujPb.js
 mv SPujPb.* ../../components
