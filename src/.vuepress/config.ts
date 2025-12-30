de43dbcf89f5220ded8b649fdc9fbc3b481cf3e4
brangpd <brangpd@outlook.com>
2025-12-30 20:36:45 +0800

    修改网站在搜索引擎中的一句话描述。

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -21,7 +21,7 @@ export default defineUserConfig({
   base: process.env.VUE_APP_BASEURL || "/",
   lang: "zh-CN",
   title: "潮汕方言白话字辞典 (Beta)",
-  description: "开源的轻量级潮汕方言辞典，兼容各地语音，支持潮拼与白话字两种拼音方案，查询响应迅速。",
+  description: "开源的轻量级潮汕方言辞典，兼容各地语音。",
   alias: {
     '@src': path.resolve(__dirname, ".."),
     '@components': path.resolve(__dirname, "components"),
